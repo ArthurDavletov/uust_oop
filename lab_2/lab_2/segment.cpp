c968ebbd4e8dd202409e8303b57ba38c8b12a2cf
Артур <ar.davletov2013@gmail.com>
2026-04-02 01:17:45 +0500

    Кружочки были добавлены

---
+++ lab_2/lab_2/segment.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <format>
+#include "segment.h"
+#include "point.h"
+
+Segment::Segment() {
+  std::cout << "Вызван конструктор по умолчанию для Segment\n";
+}
+
+Segment::~Segment() {
+  std::cout << "Вызван деструктор для Segment\n";
+}
+
+Segment::Segment(double x1, double y1, double x2, double y2)
+    : p1_(Point(x1, y1)), p2_(Point(x2, y2)) {
+  std::cout << "Вызван конструктор с параметрами (4 числа) для Segment\n";
+}
+
+Segment::Segment(const Point& p1, const Point& p2) : p1_(p1), p2_(p1) {
+  std::cout << "Вызван конструктор с параметрами (2 точки) для Segment\n";
+}
+
+double Segment::GetLength() const {
+  std::cout << "Вызван метод GetLength для Segment\n";
+  double dx = p2_.GetX() - p1_.GetX();
+  double dy = p2_.GetY() - p1_.GetY();
+  return std::sqrt(dx * dx + dy * dy);
+}
+
+std::string Segment::ToString() const {
+  std::cout << "Вызван метод ToString для Segment\n";
+  return std::format("Segment({}, {})", p1_.ToString(), p2_.ToString());
+}
---
+++ lab_2/lab_2/segment.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include "shape.h"
+#include "point.h"
+
+class Segment : public Shape {
+public:
+  Segment();
+  ~Segment() override;
+  Segment(double x1, double y1, double x2, double y2);
+  Segment(const Point& p1, const Point& p2);
+  double GetLength() const;
+  std::string ToString() const override;
+private:
+  Point p1_;
+  Point p2_;
+};
