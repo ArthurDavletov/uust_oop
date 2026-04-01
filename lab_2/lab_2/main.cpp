5539765ec509370522a1b45e5e5c7a6756e41e5a
Артур <ar.davletov2013@gmail.com>
2026-04-02 01:17:55 +0500

    Тесты с кругами

--- lab_2/lab_2/main.cpp
+++ lab_2/lab_2/main.cpp
@@ -2,15 +2,27 @@
 #include <windows.h>
 
 #include "point.h"
+#include "segment.h"
 
+void PointCheck() {
+  Point* point = new Point(1, 2);
+  delete point;
+}
+
+void SegmentCheck() {
+  Segment* segment = new Segment(1, 2, 3, 4);
+  delete segment;
+}
 
 int main() {
   // Исправление русского языка в консоли Windows
   SetConsoleCP(CP_UTF8);
   SetConsoleOutputCP(CP_UTF8);
+  PointCheck();
+  std::cout << "==========\n";
+  SegmentCheck();
 
-  Point* point = new Point(1, 2);
   std::cout << "Hello World!\n";
   std::cout << "Привет, мир!\n";
-  delete point;
+  std::cout << std::endl;
 }
