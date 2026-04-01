ada9fd4550451f8e65628fe4ae984369c2c34c32
Артур <ar.davletov2013@gmail.com>
2026-04-02 01:29:58 +0500

    Добавлен квадрат

---
+++ lab_2/lab_2/sqaure.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include "point.h"
+#include "closedshape.h"
+
+class Square : public ClosedShape {
+public:
+  Square();
+  Square(const Point& center_, double side_length);
+  ~Square() override = default;
+  std::string ToString() const override;
+  double Area() const override;
+  double Perimeter() const override;
+private:
+  Point center_;
+  double side_length_;
+};
